623d998913479ae4e69ff97f9bee61c9aea4c9bc
Changhyun Choi <changhyun.choi.98@gmail.com>
2026-05-22 16:04:25 +0900

    [Passed] 실수 받아 그대로 출력 (2ms, 0MB)

---
+++ Codetree/trail0/실수 받아 그대로 출력/enter-real-value-and-print.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    double num;
+
+    cout << fixed;
+    cout.precision(2);
+
+    cin >> num;
+    cout << num;
+    return 0;
+}
