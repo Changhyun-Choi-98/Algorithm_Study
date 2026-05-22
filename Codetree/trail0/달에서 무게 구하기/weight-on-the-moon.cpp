00d3e953d602738a0efdbdc55838713f8ccfd92e
Changhyun Choi <changhyun.choi.98@gmail.com>
2026-05-22 15:59:41 +0900

    [Passed] 달에서 무게 구하기 (1ms, 0MB)

---
+++ Codetree/trail0/달에서 무게 구하기/weight-on-the-moon.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+
+    cout << fixed;
+    cout.precision(6);
+
+    int weight = 13;
+    double ratio = 0.165;
+
+    cout << weight << " * " << ratio << " = " << weight * ratio;
+    
+    return 0;
+}
