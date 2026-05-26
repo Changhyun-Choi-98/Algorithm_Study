0e7308137317bceb82dcc02e1465dc4ac637a5ba
Changhyun Choi <changhyun.choi.98@gmail.com>
2026-05-26 10:30:24 +0900

    [Passed] 더 긴 문자열 (2ms, 0MB)

---
+++ Codetree/trail0/더 긴 문자열/longer-string.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    string a, b;
+    cin >> a >> b;
+    int a_len = a.length();
+    int b_len = b.length();
+    if (a_len > b_len) {
+        cout << a << " " << a_len;
+    }
+    else if (a_len < b_len) {
+        cout << b << " " << b_len;
+    }
+    else {
+        cout << "same";
+    }
+    return 0;
+}
