d2b36c43e95337ed726cf6c926faebc679f4e411
Changhyun Choi <changhyun.choi.98@gmail.com>
2026-05-27 11:02:51 +0900

    [Passed] 0 나올때까지 출력 (2ms, 0MB)

---
+++ Codetree/trail0/0 나올때까지 출력/print-until-zero-is-given.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    while (true) {
+        int tmp;
+        cin >> tmp;
+        if (tmp == 0) {
+            break;
+        }
+        cout << tmp << endl;
+    }
+    return 0;
+}
