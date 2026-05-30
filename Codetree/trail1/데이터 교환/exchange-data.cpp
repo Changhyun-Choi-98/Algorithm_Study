64036a8cf7a8c3e5ff98443150da1c7f215ed164
Changhyun Choi <changhyun.choi.98@gmail.com>
2026-05-30 16:52:44 +0900

    [Passed] 데이터 교환 (1ms, 0MB)

---
+++ Codetree/trail1/데이터 교환/exchange-data.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a = 5;
+    int b = 6;
+    int c = 7;
+
+    int tmp;
+
+    // tmp <- c <- b <- a <- tmp
+
+    tmp = c;
+    c = b;
+    b = a;
+    a = tmp;
+
+    cout << a << endl << b << endl << c;
+
+    return 0;
+}
